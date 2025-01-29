378bdec533f632b4976259ac0d5a03ed5fb14858
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-01-29 09:53:00 -0300

    Serviço para remover protudos

--- backend/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ backend/src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -38,4 +38,9 @@ public class ProdutoServico {
             }
         }
     }
+    public ResponseEntity<RespostaModelo> remover(long codigo){
+        pr.deleteById(codigo);
+        rm.setMensagem("O produto foi removido com sucesso!");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
+    }
 }
