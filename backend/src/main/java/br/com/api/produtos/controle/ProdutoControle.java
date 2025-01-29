23535453ae378b6830ffe1f1b8c9a7e0044d5930
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-01-29 10:12:03 -0300

    CORS resolvido

--- backend/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ backend/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -8,6 +8,7 @@ import br.com.api.produtos.servico.ProdutoServico;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
 
     @Autowired
