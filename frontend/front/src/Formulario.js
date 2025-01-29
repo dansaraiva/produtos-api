11437bf87a86fe798ccd9aaedea6f20430cfc07e
Daniel Saraiva <saraivadaniel@protonmail.com>
2025-01-29 11:25:55 -0300

    Criando componentes

---
+++ frontend/front/src/Formulario.js
@@ -0,0 +1,8 @@
+function Formulario(){
+    return(
+        <h1> Formulário</h1>
+    )
+    
+}
+
+export default Formulario;
---
+++ frontend/front/src/Tabela.js
@@ -0,0 +1,6 @@
+function Tabela(){
+    return (
+        <h1> Tabela</h1>
+    )
+}
+export default Tabela;
